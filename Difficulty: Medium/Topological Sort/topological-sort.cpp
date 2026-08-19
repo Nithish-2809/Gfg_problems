23445c3f803ca7201e010babb78b9b299a925ba3
Nithish-2809 <nithishreddy2809@gmail.com>
2026-08-19 23:52:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,43 +1,48 @@
+void dfs(int node,stack<int>&st,vector<int>&visited,const vector<vector<int>>&adj) {
+    visited[node] = 1;
+    
+    for(auto it : adj[node]) {
+        if(!visited[it]) {
+            dfs(it,st,visited,adj);
+        }
+    }
+    
+    st.push(node);
+}
+
+
+
 
 class Solution {
-    public:
-      vector<int> topoSort(int V, vector<vector<int>>& edges) {
-          // code here
-          vector<vector<int>>adj(V);
-
-          vector<int>indegree(V);
-
-          for(auto it : edges) {
-              int u = it[0];
-              int v = it[1];
-
-              adj[u].push_back(v);
-              indegree[v]++;
-          }
-
-          vector<int>ans;
-          queue<int>q;
-
-          for(int i=0;i<V;i++) {
-              if(indegree[i]==0) {
-                  q.push(i);
-              }
-          }
-
-          while(!q.empty()) {
-              int el = q.front();
-              q.pop();
-              ans.push_back(el);
-
-              for(auto it : adj[el]) {
-                  indegree[it]--;
-
-                  if(indegree[it]==0) {
-                      q.push(it);
-                  }
-              }
-          }
-
-          return ans;
-      }
-  };
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<int>>adj(V);
+        
+        for(auto it : edges) {
+            int u = it[0];
+            int v = it[1];
+            
+            adj[u].push_back(v);
+        }
+        
+        vector<int>visited(V,0);
+        
+        stack<int>st;
+        
+        for(int i=0;i<V;i++) {
+            if(!visited[i]) {
+                dfs(i,st,visited,adj);
+            }
+        }
+        
+        vector<int>ans;
+        
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        
+        return ans;
+    }
+};
