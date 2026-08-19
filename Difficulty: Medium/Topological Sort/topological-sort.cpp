2ca13c58944fa7cfb5144679c57152a768a81b6b
Nithish-2809 <nithishreddy2809@gmail.com>
2026-08-19 21:29:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,47 +1,43 @@
+
 class Solution {
-  public:
-    vector<int> topoSort(int V, vector<vector<int>>& edges) {
-        
-    
-        vector<vector<int>> adj(V);
-        for(auto it : edges) {
-            int u = it[0];
-            int v = it[1];
-            adj[u].push_back(v); 
-        }
-        
-        
-        vector<int> inDegree(V, 0);
-        for(int i = 0; i < V; i++) {
-            for(auto it : adj[i]) {
-                inDegree[it]++;
-            }
-        }
-        
-    
-        queue<int> q;
-        for(int i = 0; i < V; i++) {
-            if(inDegree[i] == 0) {
-                q.push(i);
-            }
-        }
-        
-    
-        vector<int> ans;
-        while(!q.empty()) {
-            int node = q.front();
-            q.pop();
-            
-            ans.push_back(node);
-            
-            for(auto it : adj[node]) {
-                inDegree[it]--;
-                if(inDegree[it] == 0) {
-                    q.push(it);
-                }
-            }
-        }
-        
-        return ans;
-    }
-};
+    public:
+      vector<int> topoSort(int V, vector<vector<int>>& edges) {
+          // code here
+          vector<vector<int>>adj(V);
+
+          vector<int>indegree(V);
+
+          for(auto it : edges) {
+              int u = it[0];
+              int v = it[1];
+
+              adj[u].push_back(v);
+              indegree[v]++;
+          }
+
+          vector<int>ans;
+          queue<int>q;
+
+          for(int i=0;i<V;i++) {
+              if(indegree[i]==0) {
+                  q.push(i);
+              }
+          }
+
+          while(!q.empty()) {
+              int el = q.front();
+              q.pop();
+              ans.push_back(el);
+
+              for(auto it : adj[el]) {
+                  indegree[it]--;
+
+                  if(indegree[it]==0) {
+                      q.push(it);
+                  }
+              }
+          }
+
+          return ans;
+      }
+  };
