427162bf49eded65d4f20b151799c74b14982957
Nithish-2809 <nithishreddy2809@gmail.com>
2026-08-23 18:20:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,47 +1,44 @@
-bool dfs(const vector<vector<int>>&graph,
-        vector<int>&visited,
-        int node,int parent) {
-            
-            visited[node] = 1;
-            
-            for(auto it : graph[node]) {
-                if(!visited[it]) {
-                    if(dfs(graph,visited,it,node)) return true;
-                }
-                else if(it!=parent) {
-                    return true;
-                }
-            }
-            
-        return false;
-        }
-
-
-
 class Solution {
   public:
-    bool isCycle(int V, vector<vector<int>>& edges) {
-        // Code here
-        
-        vector<vector<int>> adj(V);
+      bool isCycle(int V, vector<vector<int>>& edges) {
+          vector<vector<int>> adj(V);
 
-        for(auto &e : edges) {
-            int u = e[0];
-            int v = e[1];
-        
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        vector<int>visited(V,0);
-        
-        for(int i=0;i<V;i++) {
-            if(!visited[i]) {
-                if(dfs(adj,visited,i,-1)) {
-                    return true;
-                }
-            }
-        }
-        
-    return false;
-    }
-};
+          for(auto it : edges) {
+              int u = it[0];
+              int v = it[1];
+
+              adj[u].push_back(v);
+              adj[v].push_back(u);
+          }
+
+          queue<pair<int,int>> q;   // {node, parent}
+          vector<int> visited(V, 0);
+
+          for(int i = 0; i < V; i++) {
+
+              if(visited[i]) continue;
+
+              q.push({i, -1});
+              visited[i] = 1;
+
+              while(!q.empty()) {
+                  int node = q.front().first;
+                  int parent = q.front().second;
+                  q.pop();
+
+                  for(auto it : adj[node]) {
+
+                      if(!visited[it]) {
+                          visited[it] = 1;
+                          q.push({it, node});
+                      }
+                      else if(it != parent) {
+                          return true;
+                      }
+                  }
+              }
+          }
+
+          return false;
+      }
+  };
